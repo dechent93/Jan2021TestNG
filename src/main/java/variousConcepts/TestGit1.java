e6f49112adeb8df62b1366d662da03288d63fa80
dechen93 <dechentshering93@gmail.com>
2021-05-10 16:15:59 -0500

    fourth commit

--- src/main/java/variousConcepts/TestGit1.java
+++ src/main/java/variousConcepts/TestGit1.java
@@ -6,15 +6,17 @@ public class TestGit1 {
 	
 	public static void main(String[]args) {
 		printMe();
-		printMe1();
+		printMe2();
 		
 	}
 	public static void printMe() {
 		System.out.println("test git");
 
 }
-	public static void printMe1() {
+	public static void printMe2() {
 		System.out.println("test git 1");
+		System.out.println("test git 2");
+
 		
 	}
 }
